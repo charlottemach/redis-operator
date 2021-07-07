ba01cac5d076a77bba4efc1377e55bf04a37ae65
Aleksandr Guljajev <aguljajev@pm.me>
2021-07-07 11:48:33 +0200

    Adds missing files

---
+++ controllers/redis_manager.go
@@ -0,0 +1,226 @@
+package controllers
+
+import (
+	"errors"
+	"reflect"
+	"strconv"
+
+	//"golang.org/x/tools/godoc/redirect"
+
+	//v1 "k8s.io/client-go/tools/clientcmd/api/v1"
+
+	"context"
+	"fmt"
+
+	redisclient "github.com/go-redis/redis/v8"
+
+	//"golang.org/x/tools/godoc/redirect"
+
+	corev1 "k8s.io/api/core/v1"
+
+	// "k8s.io/apiextensions-apiserver/pkg/client/clientset"
+
+	"k8s.io/apimachinery/pkg/types"
+
+	//v1 "k8s.io/client-go/tools/clientcmd/api/v1"
+
+	controllerruntime "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	"github.com/containersolutions/redis-operator/api/v1alpha1"
+	redis "github.com/containersolutions/redis-operator/internal/redis"
+	"k8s.io/apimachinery/pkg/labels"
+)
+
+func (r *RedisClusterReconciler) StatefulSetChanges(ctx context.Context, redisCluster *v1alpha1.RedisCluster) error {
+	var err error
+
+	redisSecret, err := r.GetRedisSecret(redisCluster)
+	if err != nil {
+		r.Log.Info("ConfigureRedisCluster - secret not found", "name", redisCluster.GetName())
+		return err
+	}
+	readyNodes, _ := r.GetReadyNodes(ctx, redisCluster)
+	if !reflect.DeepEqual(readyNodes, redisCluster.Status.Nodes) {
+		r.ClusterMeet(ctx, readyNodes, redisSecret)
+		r.Recorder.Event(redisCluster, "Normal", "ClusterMeet", "Redis cluster meet completed.")
+	}
+
+	if len(readyNodes) == int(redisCluster.Spec.Replicas) {
+		r.AssignSlots(ctx, readyNodes, redisSecret)
+		r.Recorder.Event(redisCluster, "Normal", "SlotAssignment", "Slot assignment execution complete")
+	}
+
+	return nil
+}
+
+func (r *RedisClusterReconciler) RedisClusterChanges(ctx context.Context, redisCluster *v1alpha1.RedisCluster) error {
+	var err error
+
+	if err != nil {
+		r.Log.Info("ConfigureRedisCluster - secret not found", "name", redisCluster.GetName())
+		return err
+	}
+	// todo: process error
+	sset_err, sset := r.FindExistingStatefulSet(ctx, controllerruntime.Request{NamespacedName: types.NamespacedName{Name: redisCluster.Name, Namespace: redisCluster.Namespace}})
+	if sset_err != nil {
+		return err
+	}
+	currSsetReplicas := *(sset.Spec.Replicas)
+	if redisCluster.Spec.Replicas < currSsetReplicas {
+		// downscaling, migrate nodes
+		r.Log.Info("redisCluster.Spec.Replicas < sset.Spec.Replicas, downscaling", "rc.s.r", redisCluster.Spec.Replicas, "sset.s.r", currSsetReplicas)
+
+		// get the last replica of the sset
+		redisNodes, _ := r.GetReadyNodes(ctx, redisCluster)
+		// update sset replicas to current - 1
+
+		for _, node := range redisNodes {
+			r.Log.Info("Checking pod name", "podname", node.NodeName, "generated name", fmt.Sprintf("%s-%d", redisCluster.Name, currSsetReplicas-1))
+			if node.NodeName == fmt.Sprintf("%s-%d", redisCluster.Name, currSsetReplicas-1) {
+				r.Log.Info("RedisClusterUpdate, found last pod", "podName", node.NodeName)
+				// this pod gets removed, but first migrate all slots, once this done, update sset replicas count
+				// todo migrate slot
+				r.MigrateSlots(ctx, node, redisCluster)
+				newSize := currSsetReplicas - 1
+				sset.Spec.Replicas = &newSize
+				r.Log.Info("RedisClusterUpdate - updating sset count", "newsize", newSize)
+				r.Client.Update(ctx, sset)
+				break
+			}
+		}
+	}
+
+	return nil
+}
+
+func (r *RedisClusterReconciler) MigrateSlots(ctx context.Context, src_node *v1alpha1.RedisNode, redisCluster *v1alpha1.RedisCluster) {
+	// get current slot range served by the node
+	// for each slot, set status importing / migrating
+	// in round robin fashion, migrate the slot to another cluster node
+	// note: this operation should be able to resume
+	secret, _ := r.GetRedisSecret(redisCluster)
+	client := r.GetRedisClient(ctx, src_node.IP, secret)
+	slots := client.ClusterSlots(ctx).Val()
+	for _, v := range slots {
+		for slot := v.Start; slot < v.End; slot++ {
+
+			client.Do(ctx, "cluster", "setslot", slot, "importing", src_node.NodeID).Err()
+
+		}
+	}
+}
+
+func (r *RedisClusterReconciler) isOwnedByUs(o client.Object) bool {
+	labels := o.GetLabels()
+	if _, found := labels[redis.RedisClusterLabel]; found {
+		return true
+	}
+	return false
+}
+
+func (r *RedisClusterReconciler) ClusterMeet(ctx context.Context, nodes map[string]*v1alpha1.RedisNode, secret string) {
+	r.Log.Info("ClusterMeet", "nodes", nodes)
+	if len(nodes) == 0 {
+		return
+	}
+	var node *v1alpha1.RedisNode
+	rdb := r.GetRedisClient(ctx, node.IP, secret)
+	for _, v := range nodes {
+		if node == nil {
+			node = v
+		}
+		r.Log.Info("Running cluster meet", "node", node, "endpoint")
+		err := rdb.ClusterMeet(ctx, v.IP, strconv.Itoa(redis.RedisCommPort)).Err()
+		if err != nil {
+			r.Log.Error(err, "clustermeet failed", "nodes", nodes)
+		}
+	}
+}
+
+//TODO: check how many cluster slots have been already assign, and rebalance cluster if necessary
+func (r *RedisClusterReconciler) AssignSlots(ctx context.Context, nodes map[string]*v1alpha1.RedisNode, secret string) {
+	// when all nodes are formed in a cluster, addslots
+	r.Log.Info("ClusterMeet", "nodes", nodes)
+	slots := redis.SplitNodeSlots(len(nodes))
+	i := 0
+	for _, node := range nodes {
+		rdb := r.GetRedisClient(ctx, node.IP, secret)
+		rdb.ClusterAddSlotsRange(ctx, slots[i].Start, slots[i].End)
+		r.Log.Info("Running cluster assign slots", "pods", nodes)
+		i++
+	}
+}
+
+func (r *RedisClusterReconciler) GetRedisClient(ctx context.Context, ip string, secret string) *redisclient.Client {
+	rdb := redisclient.NewClient(&redisclient.Options{
+		Addr:     fmt.Sprintf("%s:%d", ip, redis.RedisCommPort),
+		Password: secret,
+		DB:       0,
+	})
+	return rdb
+}
+
+func (r *RedisClusterReconciler) GetRedisClusterPods(ctx context.Context, clusterName string) *corev1.PodList {
+	allPods := &corev1.PodList{}
+	labelSelector := labels.SelectorFromSet(
+		map[string]string{
+			redis.RedisClusterLabel: clusterName,
+			"app":                   "redis",
+		},
+	)
+
+	r.Client.List(ctx, allPods, &client.ListOptions{
+		LabelSelector: labelSelector,
+	})
+
+	return allPods
+}
+
+func (r *RedisClusterReconciler) GetReadyNodes(ctx context.Context, redisCluster *v1alpha1.RedisCluster) (map[string]*v1alpha1.RedisNode, error) {
+	allPods := &corev1.PodList{}
+	labelSelector := labels.SelectorFromSet(
+		map[string]string{
+			redis.RedisClusterLabel: redisCluster.GetName(),
+			"app":                   "redis",
+		},
+	)
+
+	r.Client.List(ctx, allPods, &client.ListOptions{
+		LabelSelector: labelSelector,
+	})
+	readyNodes := make(map[string]*v1alpha1.RedisNode, 0)
+	redisSecret, _ := r.GetRedisSecret(redisCluster)
+	for _, pod := range allPods.Items {
+		r.Log.Info("All pods list", "pod", pod.GetName(), "labels", pod.Labels)
+		for _, s := range pod.Status.Conditions {
+			if s.Type == corev1.PodReady && s.Status == corev1.ConditionTrue {
+				r.Log.Info("Pod status ready", "podname", pod.Name, "conditions", pod.Status.Conditions)
+				// get node id
+				redisClient := r.GetRedisClient(ctx, pod.Status.PodIP, redisSecret)
+				nodeId := redisClient.Do(ctx, "cluster", "myid").Val()
+				if nodeId == nil {
+					return nil, errors.New("Can't fetch node id")
+				}
+				readyNodes[nodeId.(string)] = &v1alpha1.RedisNode{IP: pod.Status.PodIP, NodeName: pod.GetName(), NodeID: nodeId.(string)}
+			}
+		}
+	}
+
+	return readyNodes, nil
+}
+
+func (r *RedisClusterReconciler) GetRedisSecret(redisCluster *v1alpha1.RedisCluster) (string, error) {
+	if redisCluster.Spec.Auth.SecretName == "" {
+		r.Log.Info("GetRedisSecret - no secret name is set in the cluster, exiting")
+		return "", nil
+	}
+
+	secret := &corev1.Secret{}
+	err := r.Client.Get(context.TODO(), types.NamespacedName{Name: redisCluster.Spec.Auth.SecretName, Namespace: redisCluster.Namespace}, secret)
+	if err != nil {
+		return "", err
+	}
+	redisSecret := string(secret.Data["requirepass"])
+	return redisSecret, nil
+}
---
+++ controllers/rediscluster_reconciler.go
@@ -0,0 +1,119 @@
+/*
+Copyright 2021.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controllers
+
+import (
+
+	//"golang.org/x/tools/godoc/redirect"
+
+	//v1 "k8s.io/client-go/tools/clientcmd/api/v1"
+
+	"context"
+
+	"sigs.k8s.io/controller-runtime/pkg/builder"
+	"sigs.k8s.io/controller-runtime/pkg/event"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
+	"sigs.k8s.io/controller-runtime/pkg/predicate"
+
+	finalizer "github.com/containersolutions/redis-operator/internal/finalizers"
+	"github.com/go-logr/logr"
+
+	//"golang.org/x/tools/godoc/redirect"
+	v1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/client-go/tools/record"
+
+	// "k8s.io/apiextensions-apiserver/pkg/client/clientset"
+
+	"k8s.io/apimachinery/pkg/runtime"
+
+	//v1 "k8s.io/client-go/tools/clientcmd/api/v1"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	"github.com/containersolutions/redis-operator/api/v1alpha1"
+
+	"sigs.k8s.io/controller-runtime/pkg/source"
+)
+
+// RedisClusterReconciler reconciles a RedisCluster object
+type RedisClusterReconciler struct {
+	client.Client
+	Log        logr.Logger
+	Scheme     *runtime.Scheme
+	Recorder   record.EventRecorder
+	Finalizers []finalizer.Finalizer
+}
+
+//+kubebuilder:rbac:groups=redis.containersolutions.com,resources=redisclusters,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=redis.containersolutions.com,resources=redisclusters/status,verbs=get;update;patch
+//+kubebuilder:rbac:groups=redis.containersolutions.com,resources=redisclusters/finalizers,verbs=update
+//+kubebuilder:rbac:groups="",resources=events,verbs=create;patch
+//+kubebuilder:rbac:groups="",resources=secrets,verbs=get
+//+kubebuilder:rbac:groups=apps,resources=statefulsets;deployments,verbs=get;list;watch
+//+kubebuilder:rbac:groups="",resources=configmap;services;pods,verbs=get;list;watch
+
+//+kubebuilder:rbac:groups="",resources=configmaps;services,verbs=get;list;create;delete
+//+kubebuilder:rbac:groups=apps,resources=statefulsets;deployments,verbs=create;delete;patch;update
+
+func (r *RedisClusterReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	_ = r.Log.WithValues("rediscluster", req.NamespacedName)
+	r.Log.Info("RedisCluster reconciler called", "name", req.Name, "ns", req.Namespace)
+	redisCluster := &v1alpha1.RedisCluster{}
+	err := r.Client.Get(ctx, req.NamespacedName, redisCluster)
+
+	if err == nil {
+		r.Log.Info("Found RedisCluster", "name", redisCluster.GetName(), "GVK", redisCluster.GroupVersionKind().String())
+		return r.ReconcileClusterObject(ctx, req, redisCluster)
+	} else {
+		// cluster deleted
+		r.Log.Info("Can't find RedisCluster, probably deleted")
+		return ctrl.Result{}, client.IgnoreNotFound(err)
+	}
+}
+
+// SetupWithManager sets up the controller with the Manager.
+func (r *RedisClusterReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&v1alpha1.RedisCluster{}).
+		Watches(&source.Kind{Type: &corev1.ConfigMap{}}, &handler.EnqueueRequestForObject{}, builder.WithPredicates(r.PreFilter())).
+		Owns(&v1.StatefulSet{}).
+		Complete(r)
+}
+
+func (r *RedisClusterReconciler) PreFilter() predicate.Predicate {
+	return predicate.Funcs{
+		UpdateFunc: func(e event.UpdateEvent) bool {
+			if !r.isOwnedByUs(e.ObjectNew) {
+				return false
+			}
+			return true
+		},
+		CreateFunc: func(e event.CreateEvent) bool {
+			if !r.isOwnedByUs(e.Object) {
+				return false
+			}
+			return true
+		},
+		DeleteFunc: func(e event.DeleteEvent) bool {
+			if !r.isOwnedByUs(e.Object) {
+				return false
+			}
+			return true
+		},
+	}
+}
